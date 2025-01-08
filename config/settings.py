f1dcefb3dab28df2df6c9a766412b7faa346d804
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2025-01-08 13:08:53 -0500

    psaume 1

--- config/settings.py
+++ config/settings.py
@@ -247,10 +247,3 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'app.User'  # Remplacez 'app' par le nom de votre application
 # Configuration WhiteNoise pour servir les fichiers statiques en production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
